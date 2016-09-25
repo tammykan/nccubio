6cf6fd5fc1d6eaf3e624e75c46ab58cdcb823b15
tammykan <tammy.xtech@gmail.com>
2016-09-25 23:26:35 +0800

    Add files via upload

---
+++ hw1/hw1.R
@@ -0,0 +1,3 @@
+# the first R script for hello world
+
+print("hello world")
---
+++ hw1/hw1_105753026.R
@@ -0,0 +1,17 @@
+# read PAM1 from data
+pam1 <- read.table(file.choose(), header=TRUE)
+
+# check PAM1 data
+dim(pam1)
+str(pam1)
+
+# construct PAM250 from PAM1
+library('expm')
+pam1 <- as.matrix(pam1)/10000
+pam250 <- as.matrix(pam1) %^% 250
+pam250 <- as.matrix(pam250) * 100
+
+
+# output PAM250 as a file
+data <- iris
+write.table(data, file="pam250.txt")
