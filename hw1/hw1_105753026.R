fc1c7049450cce69f0f2d45e6a3b2655e4ce04ca
tammykan <tammy.xtech@gmail.com>
2016-09-25 23:45:53 +0800

    Add files via upload

---
+++ hw1/hw1_105753026.R
@@ -0,0 +1,19 @@
+# read PAM1 from data
+pam1 <- read.table("pam1.txt", header=TRUE)
+
+# check PAM1 data
+dim(pam1)
+str(pam1)
+
+
+# construct PAM250 from PAM1
+library('expm')
+pam1 <- as.matrix(pam1)/10000
+pam250 <- as.matrix(pam1) %^% 250
+pam250 <- as.matrix(pam250)*100
+pam250 <- round(pam250, 0)
+
+
+# output PAM250 as a file
+data <- iris
+write.table(pam250, file="pam250.txt", sep=",")
