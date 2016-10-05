c27254cf9d28bb9a70eab90dc15c2d69e581b474
tammykan <tammy.xtech@gmail.com>
2016-10-05 23:57:59 +0800

    Update hw1_105753026.R

--- hw1/hw1_105753026.R
+++ hw1/hw1_105753026.R
@@ -5,14 +5,19 @@ pam1 <- read.table("pam1.txt", header=TRUE)
 dim(pam1)
 str(pam1)
 
-
 # construct PAM250 from PAM1
-library('expm')
+#library('expm')
 pam1 <- as.matrix(pam1)/10000
-pam250 <- as.matrix(pam1) %^% 250
+#pam250 <- as.matrix(pam1) %^% 250
+
+pam250 <- as.matrix(pam1)
+
+for(x in 1:250) {
+  pam250 <- pam250 %*% pam1
+}
+
 pam250 <- as.matrix(pam250)*100
 pam250 <- round(pam250, 0)
-
 
 # output PAM250 as a file
 data <- iris
